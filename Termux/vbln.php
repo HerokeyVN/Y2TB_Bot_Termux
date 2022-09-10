b594715a897eac32426eab009fb1e09fb4ed4ba6
HerokeyVN <70248685+HerokeyVN@users.noreply.github.com>
2022-09-10 11:10:18 +0700

    Add files via upload

---
+++ Termux/vbln.php
@@ -0,0 +1,26 @@
+<?php
+@system("clear");
+//colors
+$red="\033[1;31m";
+$green="\033[1;32m";
+$yellow="\033[1;33m";
+$blue="\033[1;34m";
+$res="\033[1;35m";
+$nau="\033[1;36m";
+$while="\033[1;37m";
+//program
+$ggid="1-9v-_3Ye9SXAMvvTvlMSi-NzuQl6vfAp";
+$link = "https://raw.githubusercontent.com/HerokeyVN/VBLN_Bot_Termux/main/Termux/install.php"
+If (file_exists("install.php") == false){
+  echo $red,("Start the installer download...\n");
+  sleep(2);
+  echo $nau,("-------------------------\n"),$green;
+  @system("wget ".$link." && php install.php");
+}
+else {
+  echo $red,("Start the installer update...\n");
+  echo $nau,("-------------------------\n"),$green;
+  sleep(2);
+  @system("rm install.php && wget ".$link." && php install.php");
+};
+?>
