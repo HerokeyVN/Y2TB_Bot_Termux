0d3d5ef463f12c0c714eeb0085997bcdaf70fd31
HerokeyVN <70248685+HerokeyVN@users.noreply.github.com>
2022-09-18 14:36:16 +0700

    Update vbln.php

--- Termux/vbln.php
+++ Termux/vbln.php
@@ -10,17 +10,20 @@ $nau="\033[1;36m";
 $while="\033[1;37m";
 //program
 $ggid="1-9v-_3Ye9SXAMvvTvlMSi-NzuQl6vfAp";
-$link = "https://raw.githubusercontent.com/HerokeyVN/VBLN_Bot_Termux/main/Termux/install.php"
-If (file_exists("install.php") == false){
+$link = "https://raw.githubusercontent.com/HerokeyVN/VBLN_Bot_Termux/main/Termux/install.php";
+if (file_exists("install.php") == false){
   echo $red,("Start the installer download...\n");
-  sleep(2);
+  //sleep(2);
   echo $nau,("-------------------------\n"),$green;
-  @system("wget ".$link." && php install.php");
+  @system("wget ".$link." && php install.php", $exitcode);
+  //echo $exitcode;
 }
 else {
   echo $red,("Start the installer update...\n");
   echo $nau,("-------------------------\n"),$green;
-  sleep(2);
-  @system("rm install.php && wget ".$link." && php install.php");
+  //sleep(2);
+  @system("rm ./install.php && wget ".$link." && php install.php", $exitcode);
 };
+if($exitcode != 0)
+	echo $nau,("-------------------------\n").$red."Can't connect to Github.com. Please check the internet and try again late!";
 ?>
