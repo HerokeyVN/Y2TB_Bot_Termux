4c2e5ca110a655807229e5d989e6017be4cc98f5
HerokeyVN <70248685+HerokeyVN@users.noreply.github.com>
2022-10-03 23:00:26 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,17 +1,17 @@
-clear
-rm ./install.sh
-echo + Setup storage:
-echo 
-echo y|termux-setup-storage
-echo 
-echo + Start update package:
-echo 
-pkg upgrade
-echo 
-echo + Install PHP
-echo 
-pkg install php -y
-echo 
-echo +Fetch install file
-echo
+clear &&
+rm ./install.sh &&
+echo + Setup storage: &&
+echo &&
+echo y|termux-setup-storage &&
+echo &&
+echo + Start update package: &&
+echo &&
+pkg upgrade &&
+echo &&
+echo + Install PHP &&
+echo &&
+pkg install php -y &&
+echo &&
+echo +Fetch install file &&
+echo &&
 curl https://raw.githubusercontent.com/HerokeyVN/VBLN_Bot_Termux/main/Termux/vbln.php --output vbln.php
